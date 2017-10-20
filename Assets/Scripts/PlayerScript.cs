2020fab22b08ae7a54fa6428b47cae6b873f9c2b
Scherbinin Andrey <ascherbinin@live.ru>
2017-10-20 14:53:46 +0500

    add jumps

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	private Rigidbody2D _rb;
+
+	// Use this for initialization
+	void Start () {
+		_rb = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetMouseButtonDown(0)) {
+			_rb.AddForce (Vector2.up * 1000);
+		}
+	}
+}

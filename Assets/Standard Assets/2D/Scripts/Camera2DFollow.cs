eaa0b839ad187fe693c5b6e319bb69ae18a6b968
Scherbinin Andrey <ascherbinin@live.ru>
2017-10-25 18:04:57 +0500

    add wall checking

---
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets._2D
+{
+    public class Camera2DFollow : MonoBehaviour
+    {
+        public Transform target;
+        public float damping = 1;
+        public float lookAheadFactor = 3;
+        public float lookAheadReturnSpeed = 0.5f;
+        public float lookAheadMoveThreshold = 0.1f;
+
+        private float m_OffsetZ;
+		private float m_StartX;
+        private Vector3 m_LastTargetPosition;
+        private Vector3 m_CurrentVelocity;
+        private Vector3 m_LookAheadPos;
+
+        // Use this for initialization
+        private void Start()
+        {
+            m_LastTargetPosition = target.position;
+            m_OffsetZ = (transform.position - target.position).z;
+			m_StartX = transform.position.x;
+            transform.parent = null;
+        }
+
+
+        // Update is called once per frame
+        private void Update()
+        {
+            m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
+            
+	
+            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
+			Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+			if (newPos.y > 0) {
+				transform.position = new Vector3 (m_StartX, newPos.y, newPos.z);
+			}
+
+			m_LastTargetPosition = target.position;
+        }
+    }
+}
